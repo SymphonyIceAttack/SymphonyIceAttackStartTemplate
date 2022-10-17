185f67a132261e34fd45e47e4395a4b441ee0bf6
SymphonyIceAttack <1994370030@qq.com>
2022-10-17 21:19:29 +0800

    Test

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+// rollup.config.js
+import babel from "rollup-plugin-babel"
+import pluginNodeResolve from "@rollup/plugin-node-resolve"
+import pluginCommonjs from "@rollup/plugin-commonjs"
+import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
+import postcss from 'rollup-plugin-postcss'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+export default {
+    input: './src/main.ts',//入口文件
+    external: ['lodash'], //告诉rollup不要将此lodash打包，而作为外部依赖
+    global: {
+        'jquery': '$' //告诉rollup 全局变量$即是jquery
+    },
+    output: {
+        file: './dist/bundle.js',
+        format: 'cjs',
+        name: 'bundleName',
+        sourcemap: true  //生成bundle.map.js文件，方便调试
+    },
+    plugins: [
+        typescript(),
+        pluginCommonjs(),
+        pluginNodeResolve(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        postcss(),
+        terser(),
+        livereload(),
+        serve({
+            open: true,
+            contentBase: 'dist'
+        })
+    ]
+}
